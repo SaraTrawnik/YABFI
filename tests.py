af24217342198f50369e2ac09c073b35a4935834
SaraTrawnik <45904168+SaraTrawnik@users.noreply.github.com>
2019-01-01 17:35:19 +0100

    get_files function + tests

--- tests.py
+++ tests.py
@@ -1,2 +1,7 @@
 import pytest
 import yabfi
+
+def test_get_file():
+  assert yabfi.get_file("test_filename") == "foo bar"
+  with pytest.raises(IOError):
+    yabfi.get_file("no_file")
--- yabfi.py
+++ yabfi.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 from re import findall
 from sys import argv
-script, filename = argv
+
+def get_file(filename: str):
+  try:
+    with open(filename, "r") as f:
+      return f.read()
+  except IOError:
+    print("file doesnt exist")
 
 if __name__ == "__main__":
-  pass
+  script, filename = argv
+  code = get_file(filename)
